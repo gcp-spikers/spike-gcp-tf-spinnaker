45d750406e4498ecc003d5009f03ce12fc982e56
Keilin Olsen <keilin.olsen@heathwallace.com>
2018-07-30 11:40:20 +1000

    add TODO for gcloud-shutdown

--- console/gcloud-shutdown.sh
+++ console/gcloud-shutdown.sh
@@ -3,9 +3,11 @@
 # Remove the cluster (and contents)
 gcloud container clusters delete spinnaker-tutorial
 
-# Remove the compute disks (TODO)
+# TODO: establish query to remove only our relevant disks
+# Remove the compute disks
 # DISKS=$(gcloud compute disks list | )
 # gcloud compute disks delete $DISKS
 
+# TODO: can we give the main service account access to remove other service accounts?
 # Remove spinnaker storage account
 # gcloud iam service-accounts delete spinnaker-storage-account@$GCLOUD_PROJECT.iam.gserviceaccount.com
