09efb0c090baf6e75c0726edef8810b359a20e8e
Keilin Olsen <keilin.olsen@heathwallace.com>
2018-07-31 11:07:35 +1000

    improve shutdown/cleanup script

--- console/gcloud-shutdown.sh
+++ console/gcloud-shutdown.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env sh
 
+# Delete spinnaker's service account
+export SA_EMAIL=$(gcloud iam service-accounts list \
+  --filter="displayName:spinnaker-storage-account" \
+  --format='value(email)')
+# TODO: can we give the main service account access to remove other service accounts?
+gcloud iam service-accounts delete $SA_EMAIL --quiet
+
 # Remove the cluster (and contents)
-gcloud container clusters delete spinnaker-tutorial
+gcloud container clusters delete spinnaker-tutorial --quiet
 
 # TODO: establish query to remove only our relevant disks
-# Remove the compute disks
-# DISKS=$(gcloud compute disks list | )
-# gcloud compute disks delete $DISKS
+# Remove the compute bucket
+export PROJECT=$(gcloud info --format='value(config.project)')
+export BUCKET=$PROJECT-spinnaker-config
+gsutil -m rm -r gs://$BUCKET
 
-# TODO: can we give the main service account access to remove other service accounts?
-# Remove spinnaker storage account
-# gcloud iam service-accounts delete spinnaker-storage-account@$GCLOUD_PROJECT.iam.gserviceaccount.com
